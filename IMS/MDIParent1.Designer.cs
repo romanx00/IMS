9b0e718dd5596ab0136b79d53a071a04acc495f7
Ryan Vu <ryangvu@csu.fullerton.edu>
2019-12-07 18:55:00 -0800

    new_user is complete,

--- IMS/MDIParent1.Designer.cs
+++ IMS/MDIParent1.Designer.cs
@@ -39,6 +39,7 @@
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
@@ -46,86 +47,100 @@
             // menuStrip
             // 
             this.menuStrip.Dock = System.Windows.Forms.DockStyle.Right;
+            this.menuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.userToolStripMenuItem,
             this.unitsToolStripMenuItem,
             this.productsToolStripMenuItem,
             this.departmentToolStripMenuItem,
             this.categoryToolStripMenuItem,
-            this.supplierToolStripMenuItem});
-            this.menuStrip.Location = new System.Drawing.Point(544, 0);
+            this.supplierToolStripMenuItem,
+            this.closeApplicationToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(1012, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(88, 453);
+            this.menuStrip.Size = new System.Drawing.Size(252, 871);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "MenuStrip";
             // 
             // userToolStripMenuItem
             // 
             this.userToolStripMenuItem.Name = "userToolStripMenuItem";
-            this.userToolStripMenuItem.Size = new System.Drawing.Size(75, 19);
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(150, 36);
             this.userToolStripMenuItem.Text = "User";
             this.userToolStripMenuItem.Click += new System.EventHandler(this.userToolStripMenuItem_Click);
             // 
             // unitsToolStripMenuItem
             // 
             this.unitsToolStripMenuItem.Name = "unitsToolStripMenuItem";
-            this.unitsToolStripMenuItem.Size = new System.Drawing.Size(75, 19);
+            this.unitsToolStripMenuItem.Size = new System.Drawing.Size(150, 36);
             this.unitsToolStripMenuItem.Text = "Units";
             this.unitsToolStripMenuItem.Click += new System.EventHandler(this.unitsToolStripMenuItem_Click);
             // 
             // productsToolStripMenuItem
             // 
             this.productsToolStripMenuItem.Name = "productsToolStripMenuItem";
-            this.productsToolStripMenuItem.Size = new System.Drawing.Size(75, 19);
+            this.productsToolStripMenuItem.Size = new System.Drawing.Size(150, 36);
             this.productsToolStripMenuItem.Text = "Products ";
             this.productsToolStripMenuItem.Click += new System.EventHandler(this.productsToolStripMenuItem_Click);
             // 
             // departmentToolStripMenuItem
             // 
             this.departmentToolStripMenuItem.Name = "departmentToolStripMenuItem";
-            this.departmentToolStripMenuItem.Size = new System.Drawing.Size(75, 19);
+            this.departmentToolStripMenuItem.Size = new System.Drawing.Size(150, 36);
             this.departmentToolStripMenuItem.Text = "Department";
             this.departmentToolStripMenuItem.Click += new System.EventHandler(this.departmentToolStripMenuItem_Click);
             // 
             // categoryToolStripMenuItem
             // 
             this.categoryToolStripMenuItem.Name = "categoryToolStripMenuItem";
-            this.categoryToolStripMenuItem.Size = new System.Drawing.Size(75, 19);
+            this.categoryToolStripMenuItem.Size = new System.Drawing.Size(150, 36);
             this.categoryToolStripMenuItem.Text = "Category";
             this.categoryToolStripMenuItem.Click += new System.EventHandler(this.categoryToolStripMenuItem_Click);
             // 
             // supplierToolStripMenuItem
             // 
             this.supplierToolStripMenuItem.Name = "supplierToolStripMenuItem";
-            this.supplierToolStripMenuItem.Size = new System.Drawing.Size(75, 19);
+            this.supplierToolStripMenuItem.Size = new System.Drawing.Size(150, 36);
             this.supplierToolStripMenuItem.Text = "Supplier";
             this.supplierToolStripMenuItem.Click += new System.EventHandler(this.supplierToolStripMenuItem_Click);
             // 
             // statusStrip
             // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 431);
+            this.statusStrip.Location = new System.Drawing.Point(0, 829);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(544, 22);
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
+            this.statusStrip.Size = new System.Drawing.Size(1012, 42);
             this.statusStrip.TabIndex = 2;
             this.statusStrip.Text = "StatusStrip";
             // 
             // toolStripStatusLabel
             // 
             this.toolStripStatusLabel.Name = "toolStripStatusLabel";
-            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(79, 32);
             this.toolStripStatusLabel.Text = "Status";
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            this.closeApplicationToolStripMenuItem.Padding = new System.Windows.Forms.Padding(8, 0, 8, 9);
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(227, 45);
+            this.closeApplicationToolStripMenuItem.Text = "Close Application";
+            this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplicationToolStripMenuItem_Click);
             // 
             // MDIParent1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 453);
+            this.ClientSize = new System.Drawing.Size(1264, 871);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.menuStrip);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "MDIParent1";
             this.Text = "MDIParent1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -151,6 +166,7 @@
         private System.Windows.Forms.ToolStripMenuItem departmentToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem categoryToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem supplierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
     }
 }
 
--- IMS/new_user.cs
+++ IMS/new_user.cs
@@ -8,13 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data.Sql;
 
 
 namespace IMS
 {
     public partial class new_user : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=IMS;Integrated Security=True");
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vhgia\Desktop\ISDS_454\IMS.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        DataTable dt;
+        int id = 0;
 
         public new_user()
         {
@@ -24,7 +29,6 @@ namespace IMS
         private void btnAddUser_Click(object sender, EventArgs e)
         {
             int i = 0;
-
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = "select * from EMPLOYEE where username ='" + txtUsername.Text + "'";
@@ -33,24 +37,36 @@ namespace IMS
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
             i = Convert.ToInt32(dt.Rows.Count.ToString());
-            if (i == 0)
+
+            if ((string.IsNullOrEmpty(txtUsername.Text) |
+                ((string.IsNullOrEmpty(txtFName.Text)) |
+                ((string.IsNullOrEmpty(txtPassword.Text)) |
+                ((string.IsNullOrEmpty(txtEmail.Text)) |
+                ((string.IsNullOrEmpty(txtContact.Text))))))))
             {
-                SqlCommand cmd1 = con.CreateCommand();
-                cmd1.CommandType = CommandType.Text;
-                cmd1.CommandText = "Insert into Employee values ('"+txtUsername.Text+ "','" + txtFName.Text + "','" + txtLName.Text + "','" + txtPassword.Text + "','" + txtEmail.Text + "','" + txtContact.Text + "') ";
-                cmd1.ExecuteNonQuery();
-                //clears txt 
-                txtUsername.Text = txtPassword.Text = txtFName.Text = txtLName.Text = txtEmail.Text = txtContact.Text = ("");
-                display();
-
-                // added mesg 
-                MessageBox.Show("New user added!");
-
-
+                MessageBox.Show("One ore more field is empty please try again");
             }
             else
             {
-                MessageBox.Show("Sorry that username is already taken!");
+                if (i == 0)
+                {
+                    SqlCommand cmd1 = con.CreateCommand();
+                    cmd1.CommandType = CommandType.Text;
+                    cmd1.CommandText = "Insert into Employee values ('" + txtUsername.Text + "','" + txtFName.Text + "','" + txtLName.Text + "','" + txtPassword.Text + "','" + txtEmail.Text + "','" + txtContact.Text + "') ";
+                    cmd1.ExecuteNonQuery();
+                    //clears txt 
+                    txtUsername.Text = txtPassword.Text = txtFName.Text = txtLName.Text = txtEmail.Text = txtContact.Text = ("");
+                    display();
+
+                    // added mesg 
+                    MessageBox.Show("New user added!");
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Sorry that username is already taken!");
+                }
             }
         }
 
@@ -72,14 +88,11 @@ namespace IMS
         }
         public void display()
         {
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from EMPLOYEE ";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
+            adapt = new SqlDataAdapter("SELECT * FROM EMPLOYEE", con);
+            dt = new DataTable();
+            adapt.Fill(dt);
             dataGridView1.DataSource = dt;
+            con.Close();
 
         }
 
@@ -95,7 +108,7 @@ namespace IMS
         private void btnRemove_Click(object sender, EventArgs e)
         {
             int id;
-
+            con.Open();
             id=Convert.ToInt32(dataGridView1.SelectedCells[0].Value.ToString());
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
@@ -103,12 +116,51 @@ namespace IMS
             cmd.ExecuteNonQuery();
             MessageBox.Show("User Deleted");
             display();
+            con.Close();
 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
+            id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtUsername.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtFName.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtLName.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtPassword.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtEmail.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            txtContact.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+        }
 
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "" && txtFName.Text !="" && txtLName.Text != "" &&
+                txtPassword.Text != "" && txtEmail.Text !="" && txtContact.Text != "")
+            {
+                con.Open();
+                cmd = new SqlCommand("UPDATE EMPLOYEE SET Username=@user,FName=@first,LName=@last,Password=@pass,Email=@email,Contact=@contact WHERE id=@id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@user", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@first", txtFName.Text);
+                cmd.Parameters.AddWithValue("@last", txtLName.Text);
+                cmd.Parameters.AddWithValue("@pass", txtPassword.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record updated successfully");
+                con.Close();
+                display();
+            }
+            else
+            {
+                MessageBox.Show("Please select a record");
+            }
+
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
